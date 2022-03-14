ffb490012f7288fe6983884266fe5f09b837cd43
Haoxi-Sun <aocysun@gmail.com>
2022-03-15 00:14:25 +1300

    14/03/2022

---
+++ String/20_Valid_Parenthess.py
@@ -0,0 +1,43 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+ 
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: s = "(]"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) % 2 == 1:
+            return False
+        
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()',"").replace('{}',"").replace('[]',"")
+        
+        if len(s) == 0:
+            return True
+        else:
+            return False
